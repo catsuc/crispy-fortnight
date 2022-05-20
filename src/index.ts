44be3d1da063efbcbc827811110c5ead97c5fbcb
Miguel Ângelo <miguelcg958@gmail.com>
2022-05-20 20:32:43 -0300

    refactor: move routes to a separated file

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ import { serverFactory } from './server';
 const PORT = process.env.PORT || 3333;
 const server = serverFactory();
 
-server.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 server.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}/`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import { routes } from './routes';
+
 function serverFactory(): express.Express {
   const server = express();
+
+  server.use(routes);
 
   return server;
 }
