7e2268b2c9ee33e9e582a044f3a4c3d5f1308463
BrenoHaraujo2503 <brenoharaujomorais@gmail.com>
2022-05-25 11:29:06 -0300

    feat: Add cors in server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import { routes } from './routes';
 
 function serverFactory(): express.Express {
   const server = express();
 
+  server.use(cors());
   server.use(routes);
 
   return server;
